e08aed1613861a01644aca23a679c3c19f6e8af4
Hesham Karm <hesham@anymindgroup.com>
2021-12-23 18:50:00 +0200

    fix lint

--- rest_auth/__init__.py
+++ rest_auth/__init__.py
@@ -1,12 +1,12 @@
 try:
-    from django.conf.urls import url
+    from django.conf.urls import url  # noqa
 except ImportError:
-    from django.urls import re_path as url
+    from django.urls import re_path as url  # noqa
 try:
-    from django.utils.translation import ugettext_lazy as _
+    from django.utils.translation import ugettext_lazy as _  # noqa
 except ImportError:
-    from django.utils.translation import gettext_lazy as _
+    from django.utils.translation import gettext_lazy as _  # noqa
 try:
-    from django.utils.encoding import force_text
+    from django.utils.encoding import force_text  # noqa
 except ImportError:
-    from django.utils.encoding import force_str as force_text
+    from django.utils.encoding import force_str as force_text  # noqa
